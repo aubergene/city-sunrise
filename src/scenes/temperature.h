47f5544c2c98ef916209e08cc4abc29580ad66da
Julian Burgess <jburgess29@bloomberg.net>
2019-12-07 19:29:25 +0000

    Temperature looking good!

--- src/scenes/temperature.h
+++ src/scenes/temperature.h
@@ -1,30 +1,31 @@
+// temp refers to temperature not temporary as usually the case in code
+const int tempFlashInterval = 500; // Flash interval in milliseconds
+const int tempWidth = 5; // Width of flashing area in pixels
+int tempLastFlash = 0; // Store last time flash was on
+int tempFlash = false; // Initial value of flash
+
+// Guessed suitable domain for outdoors values from here
+// https://en.wikipedia.org/wiki/Climate_of_London
+// const int MIN_TEMP = 2;
+// const int MAX_TEMP = 24;
+
+// Better values for indoors
+const int MIN_TEMP = 4;
+const int MAX_TEMP = 32;
+
 void temperature()
 {
     loop_bme680();
 
     display.print(F("Temperature: "));
     display.print(bme.temperature);
-    display.print((char)247);
+    display.print((char)247); // ° degree symbol
     display.println(F("C"));
-
-    // display.print("Pressure: ");
-    // display.print(bme.pressure / 100.0);
-    // display.println(" hPa");
-
-    // display.print("Humidity: ");
-    // display.print(bme.humidity);
-    // display.println(" %");
-
-    // display.print("Gas: ");
-    // display.print(bme.gas_resistance / 1000.0);
-    // display.println(" KOhms");
-
-    // display.display();
 
     FastLED.clear();
 
     int sat = 255;
-    int val = 200;
+    int val = 180;
 
     int margin = 30;
     int p1 = 100;
@@ -38,9 +39,25 @@ void temperature()
     // Add yellow the patch in between the gradients
     for (int i = 0; i < 160; i++)
     {
-        val = sin8(i * 1.4);
+        val = sin8(i * 1);
         int x = p1 - margin + i;
         leds[x] += CHSV(72, sat, gamma8[val]);
+    }
+
+    // Use a timer to turn on/off
+    if (millis() - tempLastFlash > tempFlashInterval) {
+        tempLastFlash = millis();
+        tempFlash = !tempFlash;
+    }
+
+    if (tempFlash) {
+        // Uncomment to use knob value instead
+        // int tempPos = mapC(knobValue, 0, 255, 0, NUM_LEDS - tempWidth);
+        int tempPos = mapC(bme.temperature, MIN_TEMP, MAX_TEMP, 0, NUM_LEDS - tempWidth);
+        for (int i = 0; i < tempWidth; i++)
+        {
+            leds[tempPos + i] = CRGB::Black;
+        }
     }
 
     FastLED.show();
