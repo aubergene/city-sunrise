47ec6ba4c7604c4f83d442943d97fcef8482d323
Julian Burgess <jburgess29@bloomberg.net>
2019-12-07 18:56:39 +0000

    Got a reasonably gradient

--- src/scenes/temperature.h
+++ src/scenes/temperature.h
@@ -1,5 +1,7 @@
 void temperature()
 {
+    loop_bme680();
+
     display.print(F("Temperature: "));
     display.print(bme.temperature);
     display.print((char)247);
@@ -19,41 +21,27 @@ void temperature()
 
     // display.display();
 
-    int hue;
-    int breakPoint = NUM_LEDS / 3;
-
     FastLED.clear();
 
     int sat = 255;
     int val = 200;
 
-    int freezePoint = 120;
+    int margin = 30;
+    int p1 = 100;
+    int p2 = NUM_LEDS - 1;
 
-    fill_gradient(leds, 0, CHSV(160, sat, val), freezePoint, CHSV(142, sat, val), SHORTEST_HUES);
-    fill_gradient(leds, freezePoint, CHSV(72, sat, val), NUM_LEDS-1, CHSV(0, sat, val), SHORTEST_HUES);
+    // Use the fill for a blue gradient
+    fill_gradient(leds, 0, CHSV(160, sat, val), p1, CHSV(160, sat, 0), SHORTEST_HUES);
+    // Use the fill for a red gradient
+    fill_gradient(leds, p1, CHSV(0, sat, 0), p2, CHSV(0, sat, val), SHORTEST_HUES);
 
-    // for (int i = freezePoint - 50; i < freezePoint + 100; i++)
-    // {
-    //     leds[i].fadeLightBy(i * 2);
-    // }
-
-    // for (int i = freezePoint - 50; i < freezePoint + 100; i++)
-    // {
-    //     // int v = freezePoint - abs(freezePoint -i);
-    //     leds[i] += CHSV(72, sat, 100);
-    // }
-
-    // for (int i = 0; i < NUM_LEDS; i++)
-    // {
-    //     int v = mapC(i, 0, NUM_LEDS/3, 255, 0);
-    //     leds[i] += CHSV(170, 200, v);
-
-    //     v = mapC(i, NUM_LEDS/3, NUM_LEDS, 0, 255);
-    //     leds[i] += CHSV(42, 200, v);
-
-    //     v = mapC(i, NUM_LEDS/2, NUM_LEDS, 0, 255);
-    //     leds[i] += CHSV(0, 200, v);
-    // }
+    // Add yellow the patch in between the gradients
+    for (int i = 0; i < 160; i++)
+    {
+        val = sin8(i * 1.4);
+        int x = p1 - margin + i;
+        leds[x] += CHSV(72, sat, gamma8[val]);
+    }
 
     FastLED.show();
 }
